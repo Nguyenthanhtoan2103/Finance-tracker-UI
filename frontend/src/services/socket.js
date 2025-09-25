7c7042cb2bc50bd2056eaeda3c31c6f8b96aeca5
Nguyenthanhtoan2103 <toanntse160451@fpt.edu.vn>
2025-09-25 20:41:59 +0700

    Update socket

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,15 +1,20 @@
 import { io } from "socket.io-client";
 
 const token = localStorage.getItem("token");
+const userId = localStorage.getItem("userId"); // Lấy userId lưu ở localStorage
 
 export const socket = io("http://www.ftracker.site:5000", {
   withCredentials: true,
-  autoConnect: false,
+  autoConnect: true, // tự động connect khi import
   auth: {
     token: token ? `Bearer ${token}` : null,
   },
+  query: {
+    userId: userId || "", // gửi userId lên server
+  },
 });
 
+// Nếu muốn vẫn giữ joinUserRoom thủ công
 export const joinUserRoom = (userId) => {
   if (socket && userId) {
     socket.emit("join", userId);
