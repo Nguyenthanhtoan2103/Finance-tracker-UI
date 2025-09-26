f8898b217bb03525629954d33b2a9fb6741ca6a9
Nguyenthanhtoan2103 <toanntse160451@fpt.edu.vn>
2025-09-26 11:57:11 +0700

    Update chart summary

--- frontend/src/components/ChartSumary.js
+++ frontend/src/components/ChartSumary.js
@@ -1,4 +1,4 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
 import { socket } from "../services/socket";
 
