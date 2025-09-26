680014461c6672c2628badca584aa2a756f67a43
Nguyenthanhtoan2103 <toanntse160451@fpt.edu.vn>
2025-09-26 11:34:29 +0700

    Fix app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// import React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
